240e632f390861493f63a405f9761b6deb4afbd1
Sunrise438 <napaoegie9@gmail.com>
2024-11-18 14:35:21 +0800

    pastvideos.html

--- Index.php
+++ Index.php
@@ -1,1 +1,29 @@
-
+<!DOCTYPE html>
+<html lang="en"></html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Fan Made Videos</title>
+</head>
+<style>
+    p{
+        background: red;
+        height: 20px;
+        width: 1550px;
+        color: #000;
+        display: flex;
+        justify-content: center;
+    }
+</style>
+<body>
+    <nav>
+        <div class="logo">Fan Made Videos</div>
+        <div class="nav-items">
+            <a href="index.html">Home</a>
+    </nav>
+    <header>
+        <p>All Right Reserves 2024</p>
+    </header>
+    <!-- MP4, WebM, and Ogg -->
+    <video controls loop src="BLACKPINK_-__How_You_Like_That__M_V(360p).mp4" width="1550"></video>
